a55ada24487a3f1045fb994c710abbbe21cd111a
Sam Pan <sampan511@gmail.com>
2015-02-21 21:44:16 -0500

    add angular js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(function(){
+	var app = angular.module('mainApp' []);
+
+	app.controller('StatsController', function()
+	{
+
+	});
+
+	// Custom element 
+	var app.directive('title', function(){
+		return {
+			restrict: 'E';
+			templateUrl: 'product-title.html';
+		}
+	})
+
+	var stats = {
+		cityVisited = 0;
+	}
+
+})
